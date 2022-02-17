c96116ad70cc1b402dcf280939910e0c13a9dc17
nitheshrayuduv <80449942+nitheshrayuduv@users.noreply.github.com>
2022-02-17 11:07:26 +0530

    Update DeployitDescriptorRegistryImpl.java

--- src/main/java/com/xebialabs/deployit/ci/server/DeployitDescriptorRegistryImpl.java
+++ src/main/java/com/xebialabs/deployit/ci/server/DeployitDescriptorRegistryImpl.java
@@ -318,7 +318,7 @@ public class DeployitDescriptorRegistryImpl implements DeployitDescriptorRegistr
 
     private Iterable<String> splitValue(String val) {
 	if (val.contains(";")) {
-		return Splitter.on(';').trimResults().omitEmptyStrings().split(val);
+	    return Splitter.on(';').trimResults().omitEmptyStrings().split(val);
 	}
         return Splitter.on('\n').trimResults().omitEmptyStrings().split(val);
     }
