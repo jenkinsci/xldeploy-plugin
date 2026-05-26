aae2e84cf4f24905ce6581815d60ee9675d24f3e
Preyanka Babu <bpreyanka@digital.ai>
2026-05-26 17:12:06 +0530

    S-124965: Added the missed assertNotEquals import

--- src/test/java/com/xebialabs/deployit/ci/RepositoryUtilsTest.java
+++ src/test/java/com/xebialabs/deployit/ci/RepositoryUtilsTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
